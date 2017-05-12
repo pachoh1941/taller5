880bb14a70c9a3fc52a2b9d97af7135cac201788
Juan Francisco <jf.hurtado10@uniandes.edu.co>
2017-05-12 09:26:47 -0500

    Se quitaron las funciones y se corren en main, falta probarlo

--- canal_ionico.c
+++ canal_ionico.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*Declaracion de funciones*/
-int contarLineas(char filename, FILE *datos);
-void cargarDatos(char filename, FILE *datos, double *arrayX, double *arrayY);
 /*Main*/
 int main()
 {
-    int num_lineas;
-    int i;
-    FILE *datos;
-    /*Archivo con los datos a leer*/
-    char archivo[] = "Canal_ionico.txt";
-    /*----------------------------*/
-    num_lineas = contarLineas(archivo, datos);
-    double *coordenadasX;
-    coordenadasX = malloc(num_lineas*sizeof(double));
-    double *coordenadasY;
-    coordenadasY = malloc(num_lineas*sizeof(double));
-    cargarDatos(archivo, datos, coordenadasX, coordenadasY);
-    for(i=0;i<num_lineas;i++){
-        printf("%f %f\n", coordenadasX[i], coordenadasY[i]);
-    }
-    return 0;
-}
-/*Funcion para leer las lineas de un archivo*/
-int contarLineas(char filename, FILE *datos)
-{
-    datos = fopen(filename, "r");
     int lineas = 0;
+    int i;
+    char ch;
+    FILE *datos;
+    /*Contar el numero de lineas del archivo*/
+    datos = fopen("Canal_ionico.txt", "r");
     while(!feof(datos))
     {
         ch = fgetc(datos);
@@ -36,19 +17,28 @@ int contarLineas(char filename, FILE *datos)
         }
     }
     fclose(datos);
-    return lineas;
-}
-/*Funcion para leer los datos del archivo y guardarlos en arrays*/
-void cargarDatos(char filename, FILE *datos, double *arrayX, double *arrayY)
-{
-    int i;
+    /*Arryas que guardan las coordenadas de los puntos*/
+    double *coordenadasX;
+    coordenadasX = malloc(num_lineas*sizeof(double));
+    double *coordenadasY;
+    coordenadasY = malloc(num_lineas*sizeof(double));
+    /*Cargas los datos a los arrays creados*/
     double x,y;
-    int lineas = contarLineas(filename, datos);
-    datos = fopen (filename, "r");
+    datos = fopen ("Canal_ionico.txt", "r");
     for(i=0;i<lineas;i++){
-        fscanf(datos, "%f %f",&x,&y);
-        arrayX[i] = x;
-        arrayY[i] = y;
+        if(datos == null){
+            printf("No se encuentra el arhcivo, o no existe");
+        }else{
+            fscanf(datos, "%f %f",&x,&y);
+            arrayX[i] = x;
+            arrayY[i] = y; 
+        }
     }    
     fclose(datos);
+    /*Imprimir los datos que se guardaron en los arrays*/
+    for(i=0;i<num_lineas;i++){
+        printf("%f %f\n", coordenadasX[i], coordenadasY[i]);
+    }
+    
+    return 0;
 }
