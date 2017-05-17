8fedc8aeaf34aa29cfc0955ff60666a357710bd7
Juan Francisco <jf.hurtado10@uniandes.edu.co>
2017-05-16 20:35:18 -0500

    Se cambian cosas de carga del archivo, sigue sin lograrse

--- canal_ionico.c
+++ canal_ionico.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*Main*/
-int main()
+int main(void)
 {
     int lineas = 0;
     int i;
@@ -9,36 +9,36 @@ int main()
     FILE *datos;
     /*Contar el numero de lineas del archivo*/
     datos = fopen("Canal_ionico.txt", "r");
-    while(!feof(datos))
-    {
+    if(!datos){
+        printf("Problemas con el archivo");
+    }else{
         ch = fgetc(datos);
         if(ch == '\n'){
             lineas++;
         }
     }
     fclose(datos);
-    /*Arryas que guardan las coordenadas de los puntos*/
+    /*Arrays que guardan las coordenadas de los puntos*/
     double *coordenadasX;
-    coordenadasX = malloc(num_lineas*sizeof(double));
+    coordenadasX = malloc(lineas*sizeof(double));
     double *coordenadasY;
-    coordenadasY = malloc(num_lineas*sizeof(double));
-    /*Cargas los datos a los arrays creados*/
+    coordenadasY = malloc(lineas*sizeof(double));
+    /*Carga los datos a los arrays creados*/
     double x,y;
     datos = fopen ("Canal_ionico.txt", "r");
     for(i=0;i<lineas;i++){
-        if(datos == null){
-            printf("No se encuentra el arhcivo, o no existe");
+        if(!datos){
+            printf("Problemas con el archivo");
         }else{
-            fscanf(datos, "%f %f",&x,&y);
-            arrayX[i] = x;
-            arrayY[i] = y; 
+            fscanf(datos, "%f %f\n",&x,&y);
+            coordenadasX[i] = x;
+            coordenadasY[i] = y; 
         }
     }    
     fclose(datos);
     /*Imprimir los datos que se guardaron en los arrays*/
-    for(i=0;i<num_lineas;i++){
+    for(i=0;i<lineas;i++){
         printf("%f %f\n", coordenadasX[i], coordenadasY[i]);
     }
-    
     return 0;
 }
