a12c1824ace32aa04a04c496e6780ade50114fa0
Juan Francisco <jf.hurtado10@uniandes.edu.co>
2017-05-25 22:48:20 -0500

    Correciones

--- plots_canal_ionico.py
+++ plots_canal_ionico.py
@@ -1,24 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #Se importan los resultados del archivo en C
-datos = np.loadtxt('Resultados.txt')
-centroX = datos[0,:]
-centroY = datos[1,:]
-radioMax = datos[2,:]
-coordenadasX = np.zeros(len(datos)-3)
-coordenadasY = np.zeros(len(datos)-3)
-for i in range(3,len(datos)-1):
-    coordenadasX[i]=datos[i,0]   
-    coordenadasY[i]=datos[i,1]
+with open('Resultados.txt', 'r') as myfile:
+    datos = myfile.readlines()
+archivo = datos[0].rstrip()
+centroX = float(datos[1])
+centroY = float(datos[2])
+radioMax = float(datos[3])
+coordenadas = np.loadtxt(archivo)
+coordenadasX = coordenadas[:,0]
+coordenadasY = coordenadas[:,1]
 #Grafica del circulo y los demas puntos
 puntos = 1000
-limsX = [np.amin(coordenadasX), np.amax(coordenadasX)]
-limsY = [np.amin(coordenadasY), np.amax(coordenadasY)]
-x = np.linspace(limsX[0],limsX[1],puntos)
-y = np.linspace(limsY[0],limsY[1],puntos)
-dondeCirculoExiste = np.where((((x*x)-centroX)+((y*y)-centroY))==radioMax)
-plt.plot(x[dondeCirculoExiste], y[dondeCirculoExiste])
-plt.scatter(coordenadasX, coordenadasY)
-plt.title('Parametros: x = '+str(centroX)+', y = '+str(centroY)+', radio = '+str(radioMax))
-plt.xlabel = ('X'+r'$\AA$')
-plt.ylabel = ('Y'+r'$\AA$')
+circulo = plt.Circle((centroX, centroY), radioMax, fill = False)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(coordenadasX, coordenadasY)
+ax.add_artist(circulo)
+ax.set_title('Parametros: x = '+str(centroX)+', y = '+str(centroY)+', radio = '+str(radioMax))
+ax.set_xlabel('X'+r'$(\AA)$')
+ax.set_ylabel('Y'+r'$(\AA)$')
+plt.savefig('circulo.png')
