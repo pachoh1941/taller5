890719603225104a4f629e2b2f9463b0c7e1e8a7
Juan Francisco <jf.hurtado10@uniandes.edu.co>
2017-05-25 22:47:45 -0500

    Reconfiguracion del metodo

--- canal_ionico.c
+++ canal_ionico.c
@@ -2,89 +2,53 @@
 #include <stdlib.h>
 #include <math.h>
 /*Declaracion de funciones*/
-double calcularRadio(double x, double y);
-double buscarRadioMaximo(double *X, double *Y, double rMolecula, int lineas, double centroX, double centroY);
+double calcularRadio(double x1, double x2, double y1, double y2, double radio_molecula);
 /*Main*/
 int main(void)
 {
-    int lineas = 0;
     int i;
-    char ch;
-    FILE *datos, *resultados;
-    /*Contar el numero de lineas del archivo*/
-    datos = fopen("Canal_ionico.txt", "r");
-    if(!datos){
-        printf("Problemas con el archivo");
-    }else{
-        ch = fgetc(datos);
-        if(ch == '\n'){
-            lineas++;
-        }
-    }
-    fclose(datos);
-    /*Arrays que guardan las coordenadas de los puntos*/
-    double *coordenadasX;
-    coordenadasX = malloc(lineas*sizeof(double));
-    double *coordenadasY;
-    coordenadasY = malloc(lineas*sizeof(double));
-    /*Carga los datos a los arrays creados*/
-    double x,y;
-    datos = fopen ("Canal_ionico.txt", "r");
-    for(i=0;i<lineas;i++){
-        if(!datos){
-            printf("Problemas con el archivo");
-        }else{
-            fscanf(datos, "%lf %lf\n",&x,&y);
-            coordenadasX[i] = x;
-            coordenadasY[i] = y; 
-        }
-    }    
-    fclose(datos);
-    /*Imprimir los datos que se guardaron en los arrays*/
-    for(i=0;i<lineas;i++){
-        printf("%lf %lf\n", coordenadasX[i], coordenadasY[i]);
-    }
     double centroX, centroY, radioMax; /*Variables que guardan el centro del circulo maximo y su radio*/
     double radio_molecula = 1; /*Radio de la molecula en Angstroms*/
-    /*Encontrar y guardar el radio maximo*/
-    radioMax = buscarRadioMaximo(coordenadasX, coordenadasY, radio_molecula,  lineas, centroX, centroY);
+    FILE *datos, *resultados,*puntos;
+    /*Proceso de carga de los datos e iterciones para hallar la solucion*/
+    double x,y,x1,y1,x2,y2;
+    double radio, radio_inicial, alpha;
+    /*Se inicializa radio_inicial arbitrariamente con un valor pequeño, tal que obligue a un primer paso*/
+    radio_inicial = 0.01;
+    double x0 = 0;
+    double y0 = 0;
+    char archivo[] = "Canal_ionico1.txt";
+    datos = fopen (archivo, "r");
+    while(!feof(datos)){
+      
+      fscanf(datos, "%f %lf\n%f %lf\n",&x1,&y1,&x2,&y2);
+      radio = calcularRadio(x1,y1,x2,y2,radio_molecula);
+      alpha = radio/radio_inicial;
+      if(alpha > 1){
+          radioMax = radio;
+          centroX = x1;
+          centroY = y1;
+      }else{
+          radioMax = radio_inicial;
+          centroX = x0;
+          centroY = y0;
+      }
+      x0 = x1;
+      y0 = y1;
+      radio_inicial = radio;
+    }    
     /*Exportar resultados al archivo "Resultados.txt"*/
     resultados = fopen("Resultados.txt","w");
+    fprintf(resultados, "%s\n", archivo);
     fprintf(resultados, "%lf\n", centroX);
     fprintf(resultados, "%lf\n", centroY);
     fprintf(resultados, "%lf\n", radioMax);
-    for(i=0,i<lineas,i++){
-        fprintf(resultados, "%lf %lf\n",coordenadasX[i], coordenadasY[i]);
-    }
     fclose(resultados);
     return 0;    
 }
 /*Funcion que calculo la distancia entre dos puntos*/
-double calcularRadio(double x, double y){
+double calcularRadio(double x1, double x2, double y1, double y2, double radio_molecula){
     double radio2;
-    radio2 = (x*x + y*y);
-    return pow(radio2, 0.5);
-}
-/*Funcion que busca el radio maximo y arroja los valores del centro del circulo*/
-double buscarRadioMaximo(double *X, double *Y, double rMolecula, int lineas, double centroX, double centroY){
-    int i,j;
-    double radio, alpha, radio_max;
-    double radio_incial = calcularRadio(X[0],Y[0]);/*Inicializar el radio*/
-    for(i=1;i<lineas;i++){
-        for(j=0;j<lineas;j++){
-            radio = calcularRadio(X[i],Y[j]);
-            alpha = radio/radio_inicial;
-            if(alpha > 1){
-                radio_max = radio-rMolecula;
-                centroX = X[i];
-                centroY = Y[j];
-            }else{
-                radio_max = radio_inicial-rMolecula;
-                centroX = X[i-1];
-                centroY = Y[j-1];
-            }
-            radio_inicial = radio;
-        }
-    }
-    return radio_max;
+    radio2 = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+    return pow(radio2, 0.5) - radio_molecula;
 }
